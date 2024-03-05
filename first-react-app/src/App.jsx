a6f0b35a21aed1b01a272a7a3ab6390ba214b6f2
Avdo Sator <avdo.sator@hotmail.com>
2024-03-05 10:22:26 +0100

    Example with providing non-string as argument

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Greeter from './Greeter'
+import Die from './Die'
 
 function App() {
   return (
     <div>
-      <Greeter name="avdo" />
-      <Greeter name="vedo" />
+      {/* <Greeter name="avdo" />
+      <Greeter name="vedo" /> */}
+      <Die numSides={20} />
+      <Die numSides={6} />
     </div>
   )
 }
