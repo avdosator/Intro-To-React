acb7a399da943dc4ea0549ebe4b224427da1a18a
Avdo Sator <avdo.sator@hotmail.com>
2024-03-06 11:22:07 +0100

    Restructure ShoppingList - Create ShoppingListItem component and make ShoppingList render it with provided data

--- first-react-app/src/ShoppingList.jsx
+++ first-react-app/src/ShoppingList.jsx
@@ -1,15 +1,17 @@
+import ShoppingListItem from "./ShoppingListItem"
+
 export default function ShoppingList({ items }) {
     return (
         <ul>
             {items.map(i => {
                 return (
-                    <li key={i.id} style={{
-                            color: i.completed ? "grey" : "red",
-                            textDecoration: i.completed ? "line-through" : "none"
-                        }}
-                    >
-                        {i.item} - {i.quantity}
-                    </li>
+                    <ShoppingListItem 
+                    key={i.id}
+                    item={i.item}
+                    quantity={i.quantity}
+                    completed={i.completed}
+                    />
+                    // <ShoppingListItem key={i.id} {...i} - We can also use spread operator if parameters have same names like arguments from here
                 )
             })}
         </ul>
---
+++ first-react-app/src/ShoppingListItem.jsx
@@ -0,0 +1,8 @@
+export default function ShoppingListItem({ item, quantity, completed }) {
+    const style = {color: completed ? "grey" : "red", textDecoration: completed ? "line-through" : "none"};
+    return (
+        <li style={style}>
+            {item} - {quantity}
+        </li>
+    )
+}
