6d255c46b6fd71fdca570eb47d7661af56241d9b
Avdo Sator <avdo.sator@hotmail.com>
2024-03-06 20:02:49 +0100

    Exercise PropertyList from course - work with nested elements and props

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -8,18 +8,29 @@ import DynamicHeading from './DynamicHeading'
 import ColorList from './ColorList'
 import Slots from './Slots'
 import ShoppingList from './ShoppingList'
+import PropertyList from './PropertyList'
 
 const data = [
   {id: 1, item: "eggs", quantity: 10, completed: true},
   {id: 2, item: "milk", quantity: 2, completed: false},
   {id: 3, item: "apple", quantity: 15, completed: false},
   {id: 4, item: "chicken", quantity: 1, completed: true}
-]
+];
+
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
 
 function App() {
   return (
     <div>
-      <ShoppingList items={data} />
+      {/* <ShoppingList items={data} /> */}
+      <PropertyList properties={properties} />
       {/* <DynamicHeading color='violet' text="This is dynamic heading" fontSize="35px" />
       <DynamicHeading color='lightblue' text="This is second dynamic heading" fontSize="20px" />
       <Greeter name="avdo" />
