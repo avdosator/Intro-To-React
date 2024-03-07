17853e621e17b0b6647afb932c27f126a8c8a92a
Avdo Sator <avdo.sator@hotmail.com>
2024-03-07 22:21:03 +0100

    Use state to re-render component after every change of state

--- first-react-app/src/Counter.jsx
+++ first-react-app/src/Counter.jsx
@@ -1,10 +1,12 @@
-// we can't increment num and render it after that like this
+import { useState } from "react";
 
 export default function Counter() {
-    let num = 0;
+
+    //watch out to not use const here because it will be changed
+    let [num, changeNum] = useState(0);
+
     function incrementNum() {
-        num += 1;
-        console.log(num);
+        changeNum(num += 1);
     }
     return (
         <div>
@@ -13,3 +15,21 @@ export default function Counter() {
         </div>
     )
 }
+
+
+
+// we can't increment num and render it after that like this
+
+// export default function Counter() {
+//     let num = 0;
+//     function incrementNum() {
+//         num += 1;
+//         console.log(num);
+//     }
+//     return (
+//         <div>
+//             <p>Counter is {num}</p>
+//             <button onClick={incrementNum}>+</button>
+//         </div>
+//     )
+// }
