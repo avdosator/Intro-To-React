5d231030aeee6e9609d5b97e0ff4a955d7e996fc
Avdo Sator <avdo.sator@hotmail.com>
2024-03-10 20:24:09 +0100

    Show common patterns for updating arrays in react

--- first-react-app/src/StateLetterClicker.jsx
+++ first-react-app/src/StateLetterClicker.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import {v4 as uuid} from "uuid"
+import { v4 as uuid } from "uuid"
 
 function pickLetter() {
     const letters = ["a", "b", "s", "z", "k", "x", "e"];
@@ -7,16 +7,25 @@ function pickLetter() {
 }
 
 export default function StateLetterClicker() {
-    const [letters, setLetters] = useState([{id: uuid(), letter: "a"}]);
+    const [letters, setLetters] = useState([{ id: uuid(), letter: "a" }]);
     function addLetter() {
         const randLetter = pickLetter();
         setLetters(oldLetters => {
-            return [...oldLetters, {id: uuid(), letter: randLetter}];
+            return [...oldLetters, { id: uuid(), letter: randLetter }];
+        })
+    }
+    function deleteLetter(id) {
+        setLetters(oldLetters => {
+            return [...oldLetters.filter((l) => l.id !== id )]
         })
     }
     return (
         <div>
-            {letters.map(l => <span key={l.id} style={{fontSize: "4rem"}}>{l.letter}</span>)} <br/>
+            {letters.map(l =>
+                <span key={l.id} style={{ fontSize: "4rem" }} onClick={() => deleteLetter(l.id)}> {/* Use callback like this when you want to send data to some onClick function */}
+                    {l.letter}
+                </span>
+            )} <br />
             <button onClick={addLetter}>Add Letter</button>
         </div>
     )
