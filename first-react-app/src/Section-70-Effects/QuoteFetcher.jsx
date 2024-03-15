163f7fd312f1d1f38936765242650e39795b7733
Avdo Sator <avdo.sator@hotmail.com>
2024-03-15 17:57:39 +0100

    Implement basic loader

--- first-react-app/src/Section-70-Effects/QuoteFetcher.jsx
+++ first-react-app/src/Section-70-Effects/QuoteFetcher.jsx
@@ -4,19 +4,23 @@ const URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
 
 export default function QuoteFetcher() {
     let [quote, setQuote] = useState({text: "", author: ""});
+    let [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+    useEffect(() => { 
         fetchQuote();
     }, [])
 
     async function fetchQuote() {
         const response = await fetch(URL);
+        setIsLoading(true);
         const jsonRes = await response.json();
         const quote = jsonRes.quote;
         setQuote(quote);
+        setIsLoading(false);
     }
     return (
         <div>
+            {isLoading && <p>Loading...</p>}
             <h2>{quote.text}</h2>
             <h3>{quote.author}</h3>
             <button onClick={fetchQuote}> New quote</button>
