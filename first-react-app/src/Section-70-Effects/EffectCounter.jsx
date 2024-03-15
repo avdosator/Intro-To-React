228d9928f01b948d178d25eb16b9804e39483ddb
Avdo Sator <avdo.sator@hotmail.com>
2024-03-15 13:29:55 +0100

    Show how to use second parameter of useEffect function

--- first-react-app/src/Section-70-Effects/EffectCounter.jsx
+++ first-react-app/src/Section-70-Effects/EffectCounter.jsx
@@ -2,17 +2,23 @@ import { useEffect, useState } from "react";
 
 export default function EffectCounter() {
     let [counter, setCounter] = useState(0);
+    let [name, setName] = useState("");
     useEffect(() => {
         console.log("Effect was called!");
-    })
+    }, [counter])
     function increment() {
         setCounter(prevCounter => prevCounter + 1);
+    }
+
+    function handleChange(e) {
+        setName(e.target.value);
     }
 
     return (
         <div>
             <p>{counter}</p>
             <button onClick={increment}>Add 1</button>
+            <input type="text" name="name" id="name" onChange={handleChange} />
         </div>
     )
 }   
