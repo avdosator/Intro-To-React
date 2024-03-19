d937cf71cc740f460c0a21f42f0cd96b9b18b011
Avdo Sator <avdo.sator@hotmail.com>
2024-03-19 22:27:11 +0100

    Use localStorage to store todos

--- Todo-List/src/TodoList.jsx
+++ Todo-List/src/TodoList.jsx
@@ -2,23 +2,30 @@ import List from '@mui/material/List';
 import TodoListItem from './TodoListItem';
 import { useState } from 'react';
 import TodoForm from './TodoForm';
+import { useEffect } from 'react';
 
-const initialTodos = () => {
-    return [
-        { id: 1, text: "Clean the house", completed: false },
-        { id: 2, text: "Walk the dog", completed: false },
-        { id: 3, text: "Feed cows", completed: false },
-        { id: 4, text: "Go to market", completed: true }
-    ]
+const getData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"));
+    if(!data) {
+        return [];
+    } else {
+        return data;
+    }      
 }
 
 export default function TodoList() {
-    let [todos, setTodos] = useState(initialTodos());
+    let [todos, setTodos] = useState(getData);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
     function removeTodo(id) {
         setTodos((oldTodos) => {
             return oldTodos.filter((todo) => (todo.id !== id));
         })
     }
+
     function toggleTodo(id) {
         setTodos((prevTodos) => {
             return prevTodos.map((todo) => {
@@ -30,9 +37,11 @@ export default function TodoList() {
             })
         })
     }
+
     function addTodo(todoText) {
         setTodos(oldTodos => [...oldTodos, {id: crypto.randomUUID(), text: todoText, completed: false}]);
     }
+
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             {todos.map((todo) =>
