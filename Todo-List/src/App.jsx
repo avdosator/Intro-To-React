b6688beed6361d6975b3c22a8ea231329dba0b13
Avdo Sator <avdo.sator@hotmail.com>
2024-03-19 16:04:11 +0100

    Destructure TodoList and create new component TodoListItem

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -2,12 +2,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import CssBaseline from "@mui/material/CssBaseline"
 import TodoList from './TodoList';
 
 function App() {
 
   return (
     <>
+      <CssBaseline />
       <h1>Todos</h1>
       <TodoList />
     </>
--- Todo-List/src/TodoList.jsx
+++ Todo-List/src/TodoList.jsx
@@ -1,55 +1,24 @@
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import TodoListItem from './TodoListItem';
 import { useState } from 'react';
+
 
 const initialTodos = () => {
     return [
         { id: 1, text: "Clean the house", completed: false },
         { id: 2, text: "Walk the dog", completed: false },
         { id: 3, text: "Feed cows", completed: false },
-        { id: 4, text: "Go to market", completed: false }
+        { id: 4, text: "Go to market", completed: true }
     ]
 }
 
 export default function TodoList() {
     let [todos, setTodos] = useState(initialTodos());
-    console.log(todos)
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             {todos.map((todo) => {
-        const labelId = `checkbox-list-label-${todo.id}`;
-
-        return (
-            <ListItem
-                key={todo.id}
-                secondaryAction={
-                    <IconButton edge="end" aria-label="comments">
-                        <CommentIcon />
-                    </IconButton>
-                }
-                disablePadding
-            >
-                <ListItemButton role={undefined} dense>
-                    <ListItemIcon>
-                        <Checkbox
-                            edge="start"
-                            checked={todo.completed}
-                            tabIndex={-1}
-                            disableRipple
-                            inputProps={{ 'aria-labelledby': labelId }}
-                        />
-                    </ListItemIcon>
-                    <ListItemText id={labelId} primary={todo.text} />
-                </ListItemButton>
-            </ListItem>
-        );
-    })}
+               return <TodoListItem key={todo.id} todo={todo}/>
+            })}
         </List>
     )
 }
