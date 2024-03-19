516c6fbde1cbbd8033d2ff6f166d9699258a5f25
Avdo Sator <avdo.sator@hotmail.com>
2024-03-19 11:43:22 +0100

    Create checkbox list without functionalities

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -2,12 +2,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import TodoList from './TodoList';
 
 function App() {
 
   return (
     <>
       <h1>Todos</h1>
+      <TodoList />
     </>
   )
 }
