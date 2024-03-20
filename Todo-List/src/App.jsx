a285c0611900b954ee898a2dd577cbaa39c7821e
Avdo Sator <avdo.sator@hotmail.com>
2024-03-20 09:32:31 +0100

    Create simple Navbar component

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -4,12 +4,14 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import CssBaseline from "@mui/material/CssBaseline"
 import TodoList from './TodoList';
+import Navbar from './Navbar';
 
 function App() {
 
   return (
     <>
       <CssBaseline />
+      <Navbar />
       <h1>Todos</h1>
       <TodoList />
     </>
