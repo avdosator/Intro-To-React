7ea5656449bbc77b5917203540c7fade10c6e35d
Avdo Sator <avdo.sator@hotmail.com>
2024-03-24 11:47:36 +0100

    Add styles through css files and also some inline styles

--- Todo-List/src/React-18-Fundamentals/Banner.jsx
+++ Todo-List/src/React-18-Fundamentals/Banner.jsx
@@ -1,12 +1,11 @@
 import "./Banner.css"
-
 export default function Banner() {
     return (
-        <header className="Banner">
-            <div>
-                <img src="https://github.com/RolandGuijt/ps-react-fundamentals/blob/Anatomy/public/GloboLogo.png?raw=true" alt="" />
+        <header className="row mb-4">
+            <div className="col-5">
+                <img  className="logo" src="https://github.com/RolandGuijt/ps-react-fundamentals/blob/Anatomy/public/GloboLogo.png?raw=true" alt="" />
             </div>
-            <div>
+            <div className="col-7 mt-5" style={{color: "coral", fontStyle: "italic", fontSize: "x-large"}}>
                 Providing houses all over the world
             </div>
         </header>
