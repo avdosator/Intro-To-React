6de9b03d53163468931828b49ee73e59769f4107
Avdo Sator <avdo.sator@hotmail.com>
2024-03-24 10:47:33 +0100

    Create Banner component for simple app from React 18 Fundamentals course

--- Todo-List/src/App.jsx
+++ Todo-List/src/App.jsx
@@ -2,18 +2,23 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import "./App.css"
 import CssBaseline from "@mui/material/CssBaseline"
 import TodoList from './TodoList';
 import Navbar from './Navbar';
+import Banner from './React-18-Fundamentals/Banner';
 
 function App() {
 
   return (
-    <>
+    <Banner />
+
+  // Todo List App
+    /*  <>
       <CssBaseline />
       <Navbar />
       <TodoList />
-    </>
+    </> */
   )
 }
 
---
+++ Todo-List/src/React-18-Fundamentals/Banner.jsx
@@ -0,0 +1,14 @@
+import "./Banner.css"
+
+export default function Banner() {
+    return (
+        <header className="Banner">
+            <div>
+                <img src="https://github.com/RolandGuijt/ps-react-fundamentals/blob/Anatomy/public/GloboLogo.png?raw=true" alt="" />
+            </div>
+            <div>
+                Providing houses all over the world
+            </div>
+        </header>
+    )
+}
